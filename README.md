4ad13481a8f5f385f1c28959e9d0e9fd18c05108
SirThana <martijn.zijl.3000@gmail.com>
2019-10-25 16:40:55 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # loginsystem
 basic loginsystem
+
+Uses MYSQL as back-end database, main.py has the code to talk to the backend,
+connection variables are hardcoded <-- fix soon
---
+++ main.py
@@ -0,0 +1,84 @@
+from werkzeug import *
+import pymysql
+import time
+from flask import *
+from flask_wtf import *
+import pdb
+
+
+
+
+app = Flask(__name__, static_folder="static")
+
+
+#   --> return a database connection
+def initConnection(user, password, db):
+    try:
+        dbconnection = pymysql.connect("localhost",user,password,db)
+        return dbconnection
+    except Exception as e:
+        print(e)
+        return False
+
+
+def selectQuery(userName):
+    dbconnection = initConnection('db1', 'password', 'db')
+    query = "SELECT * FROM Users WHERE {}=\'{}'"
+    field = 'FirstName'
+    query = query.format(field,userName)
+    try:
+        with dbconnection.cursor() as cursor:
+            cursor.execute(query)
+            result = cursor.fetchone()
+            return str(result)
+
+    except Exception as e:
+        print(e)
+
+
+#   --> Generic queries, don't use this function
+def queryDB(dbconnection, query):
+    #Insert queries
+    if query[:6] == "INSERT":
+        try:
+            with dbconnection.cursor() as cursor:
+                cursor.execute(query)
+                query = cursor.fetchone()
+                dbconnection.commit()
+                return query;
+        except Exception as e:
+            print(e)
+            return False
+
+    try:
+        with dbconnection.cursor() as cursor:
+            cursor.execute(query)
+            query = cursor.fetchall()
+            return query;
+    except Exception as e:
+        print(e)
+        return False
+
+
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+    #pdb.set_trace()
+    if request.method == "POST":
+        query = request.form #query returns a multiDict, with a key of projectFilepath
+        x = selectQuery(query['projectFilepath']) #Calling for the object at ^^^^^^^^ 
+        return x #returns the result of the selectQuery with input ^^^^^
+    else:
+        return None
+
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', name='Home')
+
+def main():
+    
+    app.run(host='192.168.1.11', debug=True)
+
+main()
