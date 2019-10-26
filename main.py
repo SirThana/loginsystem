21e101e79d6f2b746723c6911d8a651050f49b1a
SirThana <martijn.zijl.3000@gmail.com>
2019-10-26 23:00:14 +0200

    cookie functionality testing

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ import pdb
 #   1.  Clean up the code, remove uneccesary functions
 
 
-#Default variables
+#Flask variables
 app = Flask(__name__, static_folder="static")
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
 
 #   --> return a database connection
 def initConnection(user, password, db):
@@ -22,12 +23,11 @@ def initConnection(user, password, db):
         print(e)
         return False
 
-
-def selectQuery(userName):
+#   --> find a value in a field, in the fixed table Users return the result as a str
+def selectQuery(value, field):
     dbconnection = initConnection('db1', 'password', 'db')
     query = "SELECT * FROM Users WHERE {}=\'{}'"
-    field = 'Email'
-    query = query.format(field,userName)
+    query = query.format(field,value)
     try:
         with dbconnection.cursor() as cursor:
             cursor.execute(query)
@@ -38,46 +38,48 @@ def selectQuery(userName):
         print(e)
 
 
-#   --> Generic queries, don't use this function
+#   --> Generic queries, don't use this function || It's crap
 def queryDB(dbconnection, query):
-    #Insert queries
-    if query[:6] == "INSERT":
-        try:
-            with dbconnection.cursor() as cursor:
-                cursor.execute(query)
-                query = cursor.fetchone()
-                dbconnection.commit()
-                return query
-        except Exception as e:
-            print(e)
-            return False
+    try:
+        with dbconnection.cursor() as cursor:
+            cursor.execute(query)
+            query = cursor.fetchone()
+            dbconnection.commit()
+            return query
+    except Exception as e:
+        print(e)
+        return False
 
 
-    else:
-        try:
-            with dbconnection.cursor() as cursor:
-                cursor.execute(query)
-                query = cursor.fetchall()
-                return query;
-        except Exception as e:
-            print(e)
-            return False
+@app.route("/cookies")
+def cookies(ID):
+    res = make_response(render_template("/index.html"))
+    #res.set_cookie("flavor", "Chocolate chip", max_age=10, expires=None, path=request.path,
+            #domain=None, secure=False, httponly=False,  samesite=False)
+
+    res.set_cookie("ID", ID)
+    cookie = request.cookies #retrieve all cookies from user
+    print("\n\n\n", "||||||||")
+    print(cookie.get("ID")) #Get the cookie with a key of ID (cookies are dicts)
+    print(" ||||||||")
+    return res
+
 
 @app.route('/handle_data', methods=['POST'])
 def handle_data():
     if request.method == "POST":
-        query = request.form #query returns a multiDict, with a key of projectFilepath
-        x = selectQuery(query['query']) #Calling for the object at ^^^^^^^^ 
-        if x == 'None':
-            x = 'Bad E-Mail'
-            print(x)
-            flash('badEmail')
+        input = request.form #input from any form, as multi dict (key query for value)
+        if input["query"] == "Test":
+            x = 5 # << this would be a select query 
+            return cookies(str(x)) #You can return cookie functionality without routing there!
+        if input["query"] == '': #<< write logic if a query returns False || None
+            flash('Bad Credentials')
             return redirect("/")
         else:
-            return render_template('handler.html', name='Handler', data=x)
+            return render_template('index.html', name='Home')
 
 
-
+#   --> Home route
 @app.route('/')
 @app.route('/index')
 def index():
