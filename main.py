c2cdb5c9e9b79944a1cb38176cb29ca25336ff21
SirThana <martijn.zijl.3000@gmail.com>
2019-10-31 22:49:52 +0100

    final push

--- main.py
+++ main.py
@@ -2,24 +2,32 @@ from werkzeug import *
 from flask import *
 import pymysql
 import random
-import threading
-#-----------------
 import time
-import pdb
+#-----------------
 
 #TODO
 #   1.  Clean up the code, remove uneccesary functions
 #   2.  Make selectQuery returnn a dict instead, easier to parse data from that over a string |Done
 
 #UPTOSPEED
-#   Make a logout function, should overwrite user cookie with an experation date of the past,
-#   forcing the browser to remove the cookie
+#   Code up a download function, that downloads something if a user is authenticated
+#   Just a prove of concept, so i can extend on this later
 
 #Flask variables
 app = Flask(__name__, static_folder="static")
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-#global session_variables
-#session_variables = []
+
+
+@app.route('/downloadFile', methods=['POST'])
+def downloadFile():
+    x = authenticateUser()
+    print(x)
+    if x[1] == True:
+        path = "./supersecretdata.txt"
+        return send_file(path, as_attachment=True)
+    else:
+        return redirect("/")
+
 
 #   --> return a database connection
 def initConnection(user, password, db):
@@ -80,44 +88,11 @@ def storeCookie(dbconnection, cookie):
 #       Useful because we can keep this cookie in memory and compare it every time against theirs
 #       if expire is set to 0, set a cookie to expire immediately, anything other than 0, session
 @app.route("/cookies")
-def writeCookie(KEY, VALUE, expire):
+def writeCookie(KEY, VALUE):
     res = make_response(redirect("/"))
-    #res.set_cookie("flavor", "Chocolate chip", max_age=10, expires=None, path=request.path,
-            #domain=None, secure=False, httponly=False,  samesite=False)
-
-    if expire == 0:
-        res.set_cookie(KEY, VALUE, expires=0) #Set the cookie in the users browser
-        #session_variables.append(VALUE[50])
-        #session_variables.append(VALUE)
-
-    else:
-        res.set_cookie(str(KEY), VALUE) #Set the cookie in the users browser
-        #Write the cookie ID and VALUE to the session variables for thread to read
-        #session_variables.append(VALUE[50])
-        #session_variables.append(VALUE)
+    res.set_cookie(str(KEY), VALUE) #Set the cookie in the users browser
     return res #Route back to the home page
 
-
-#   --> Reads the cookie that's stored in the session_variable, compares it to the one
-#       in the database, if they match, user is authenticated
-#       ||NOTE Not very secure, if the user deletes their cookie AKA logs out,
-#       The session variable won't be updated, they'll be out of sync and this function
-#       will still report AUTHENTICATED, could solve this by making a logout buttonn
-#       That calls for a function to remove the values in session_variables
-#       Reason for this workaround is the fact that threads can't access session based objects
-#       Such as the cookie.requests
-#def authThread():
-#    while True:
-#        if len(session_variables) > 0:
-#            dbresult = selectQuery("ID", session_variables[0])
-#            try:
-#                if session_variables[1] == dbresult[5]:
-#                    #print("AUTHENTICATED", time.time())
-#                    pass
-#            except:
-#                pass
-#            time.sleep(1)
-  
 
 #   --> Authenticate a user by comparing their cookie, against the one in the database.
 #       If they match, return True, and their ID. We can use the ID to find any other data that
@@ -127,7 +102,7 @@ def authenticateUser():
         remoteCookie = request.cookies
         dbCookie = selectQuery('ID', remoteCookie['ID'][50]) #select * from Users ID == cookie[50]
     except Exception as e:
-        return False, False, 1 #Don't ask, don't tell
+        return "Not Logged in", False #Don't ask, don't tell
     
     #dbCookie = selectQuery('ID', remoteCookie['ID'][50])
     if remoteCookie['ID'] == dbCookie[5]: #Compare remote to db cookie
@@ -136,7 +111,8 @@ def authenticateUser():
         returnMe = "Logged in as: {} {}".format(credentials[1], credentials[2])
         return returnMe, True
     
-    return False
+    return "Not Logged in", False #Don't ask, don't tell
+    
 
 @app.route('/handle_data', methods=['POST'])
 def handle_data():
@@ -150,7 +126,7 @@ def handle_data():
             cookie = generateCookie(credentials[0]) #Generate cookie, ID embedded
             dbconnection = initConnection('db1', 'password', 'db')
             storeCookie(dbconnection, cookie)
-            return writeCookie("ID",cookie, -1) #write cookie, without routing
+            return writeCookie("ID",cookie) #write cookie, without routing
         else:
             flash('Bad Credentials')
             return redirect("/")
@@ -162,9 +138,9 @@ def handle_data():
 @app.route('/logout', methods=['POST'])
 def logout():
     response = Response()
-    response.delete_cookie("ID")
-    redirect("/")
+    response.delete_cookie("ID"), render_template('index.html', name='Home')
     return response
+
 
 #   --> Home route
 @app.route('/')
@@ -176,11 +152,6 @@ def index():
 
 #   --> Main, run the application in debug mode
 def main():
-    #   --> Runs authenticateUser in a thread in the background
-    #thread_Auth = threading.Thread(target=authThread, args=())
-    #thread_Auth.daemon = True
-    #thread_Auth.start()
-
 
     app.run(host='192.168.1.11', debug=True)
 
